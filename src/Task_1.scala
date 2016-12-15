a6e0896338e863b9eb9d3e7dcecbc7c758ae8df2
Ilia Tracevski 350531 <Deeo-sama@hotmail.com>
2016-12-15 11:02:05 +0300

    Empty

---
+++ src/Task_1.scala
@@ -0,0 +1,8 @@
+/*
+ * def curry[A, B, C](f: (A, B) => C): A => (B => C) 
+ * */
+
+object Task_1 {
+  def main(args: Array[String]) {
+  }
+}
---
+++ src/Task_2.scala
@@ -0,0 +1,10 @@
+/*
+* def	unfold[T,	S](z:	S)(f:	S	=>	Option[(T,	S)]):	Stream[T]
+* */
+
+import scala.collection.immutable.Stream.cons
+
+object Task_2 {
+    def main(args: Array[String]): Unit = {
+      }
+}
